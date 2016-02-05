223d699a88041cda5e562238e6549b9cc0dfaf66
Philip Adlergard <philip.adlergard@hiq.se>
2016-02-05 16:02:20 +0100

    Responsive design done and the model for post is updated with url

--- controllers/addpostController.js
+++ controllers/addpostController.js
@@ -1,3 +1,13 @@
 angular.module('app').controller('addpostController', ['$scope', '$http', function ($scope, $http) {
     
+    $scope.submit = function (isValid) {
+        
+        if (!isValid) {
+            
+            $scope.submitted = true;
+            return;
+        }
+        
+        //send to database
+    }
 }])
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,16 @@
 angular.module('app').controller('mainController', ['$scope', '$http', function ($scope, $http) {
     
-    function post(header, body) {
+    function post(header, url, text) {
         this.header = header;
-        this.body = body;
+        this.url = url;
+        this.text = text;
     }
     
-    $scope.posts = [new post('Post1', 'This is my first post')];
+    $scope.posts = [];
+    
+    $scope.posts.push(new post('Bästa sökmotorn', 'http://www.google.se', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation'));
+    
+    $scope.posts .push(new post('Post2', 'www.example.se', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'));
+    
+    $scope.posts.push(new post('Post3', 'www.example.se', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad         minim veniam, quis nostrud exercitation ullamco laboris nis.'));
 }])
